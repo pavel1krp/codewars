bbc68af8c5f4a8b4b0f2350ac8ebea69fad42699
Pavel <pavel07007@gmail.com>
2022-10-26 17:01:56 +0300

    add countODd  function solution

---
+++ CountOddNumbers.js
@@ -0,0 +1,6 @@
+function oddCount(n){
+  console.log(n)
+ if(n%2==0){ 
+ return n/2}
+return n/2 -0.5
+}
---
+++ sumsequrs.js
@@ -0,0 +1,13 @@
+let count = 0;
+let arr1 = []
+function sum(arr, n) {
+  
+  // Only change code below this line
+  if( n === 0){
+    return 0;
+  } else{
+   return sum(arr, n-1) + arr[n-1];
+  }
+  // Only change code above this line
+}
+console.log(sum([2, 3, 4,4], 2))
