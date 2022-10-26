3f2bdefd93c901b48c5ccb47b4f8d6affd3fd6a0
Pavel <pavel07007@gmail.com>
2022-10-26 19:03:44 +0300

    try some  function solution

---
+++ productArray.js
@@ -0,0 +1,10 @@
+// function productArray(numbers){
+//   let x = []
+//   for(let i=0; i< numbers.length; i++){
+//     x.push([numbers.reduce((element, kek) => 
+//       element * kek /element
+//     )])
+//   }
+//   return x
+// }
+// console.log(productArray([10,3,5,6,2]))
