2264f03c9a4934ac96151ee5bec67701e73f5ba6
Pavel <pavel07007@gmail.com>
2022-10-28 14:17:00 +0300

    add positiveSum solution

---
+++ sumofpositive.js
@@ -0,0 +1,7 @@
+function positiveSum(arr) {
+  let filtr = arr.filter(el => el> 0);
+ if(filtr.length>0){
+ return filtr.reduce((el, ell) => el+ ell)}
+ return [0]
+}
+console.log(positiveSum([2,4,5,67,4,1]))
