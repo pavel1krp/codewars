57f2a909514fbb5cdd572fca46b09dfa3e247f6c
Pavel <pavel07007@gmail.com>
2022-10-29 09:34:00 +0300

    add descendingOrder  solution

--- getCount.js
+++ getCount.js
@@ -8,12 +8,6 @@ function getCount(str) {
   }
   return sum;
 }
-// console.log(getCount('abracadabra'))
-
-// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-// let vowelRegex = /[aeiou]/gi; // Change this line
-// let result = quoteSample.match(vowelRegex); // Change this line
-// console.log(result)
 
 function getCount1(str){
   let res = str.toLowerCase().match(/[aeiou]/gi);
