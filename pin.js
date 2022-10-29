b263de807d7380f63972202e5ead467e95c71fa5
Pavel <pavel07007@gmail.com>
2022-10-29 12:47:56 +0300

    add stringtoarray solution

--- pin.js
+++ pin.js
@@ -1,3 +1,6 @@
 function validatePIN (pin) {
-  return (/^([0-9]{4}|[0-9]{6})$/).test(pin)
+ return (/^([0-9]{4}|[0-9]{6})$/).test(pin)
+}
+function validatePIN (pin) {
+  return (/^(\d{4}|\d{6})$/).test(pin)
  }
