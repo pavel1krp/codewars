a4f439e9aaa12a66f8aa86c90ee9f02193cf7e27
Pavel <pavel07007@gmail.com>
2022-10-29 08:29:16 +0300

    add getCount  solution

--- getCount.js
+++ getCount.js
@@ -1,9 +1,11 @@
 function getCount(str) {
-  let letter =['a','e','i','o','u']
-  arr = str.split('');
-  for(let i = 0; i<str.lngth; i++){
-    if(str.)
+  let letter =['a','e','i','o','u'];
+  let sum = 0;
+  for(let i of str.toLowerCase()){
+    if(letter.includes(i)){
+      sum ++;
+    }
   }
-  return arr;
+  return sum;
 }
 console.log(getCount('abracadabra'))
