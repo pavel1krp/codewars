2b55e8114ef536e3c1f76a9760bedffffa8e3dfb
Pavel <pavel07007@gmail.com>
2022-10-29 12:38:04 +0300

    add pin solution

--- GrasshopperSum.js
+++ GrasshopperSum.js
@@ -5,3 +5,8 @@ var summation = function (num) {
   }
   return arr.reduce((el,e)=> el+ e)
 }
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
+console.log(result)
