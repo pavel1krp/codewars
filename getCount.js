5ed3db782dece163a2921516b55a588b324e8903
Pavel <pavel07007@gmail.com>
2022-10-29 08:49:17 +0300

    add one more getCount   solution

--- getCount.js
+++ getCount.js
@@ -8,4 +8,18 @@ function getCount(str) {
   }
   return sum;
 }
-console.log(getCount('abracadabra'))
+// console.log(getCount('abracadabra'))
+
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/gi; // Change this line
+// let result = quoteSample.match(vowelRegex); // Change this line
+// console.log(result)
+
+function getCount1(str){
+  let res = str.toLowerCase().match(/[aeiou]/gi);
+  if (!res){
+    return 0
+  }
+  return res.length
+}
+console.log(getCount1('ssss'))
