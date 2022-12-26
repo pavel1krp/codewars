d57abfb3cdc87117bd43406cf0259bc732cd0de0
Pavel <pavel07007@gmail.com>
2022-12-26 12:48:34 +0300

    add better  solution

--- unique_order.js
+++ unique_order.js
@@ -1,12 +1,23 @@
+var uniqueInOrderWrong=function(iterable){
+  if(typeof iterable === 'string'){
+  iterable = iterable.split('')
+}
+for(let i = 0; i<iterable.length; i++){
+  if(iterable[i]===iterable[i+1]){
+     iterable.splice(i, 1,'qwertasdf')
+  }
+}
+return iterable.filter(el=> el!= 'qwertasdf')
+}
 const uniqueInOrder=function(iterable){
+  let sumArr = []
   if(typeof iterable === 'string'){
     iterable = iterable.split('')
   }
   for(let i = 0; i<iterable.length; i++){
-    if(iterable[i]===iterable[i+1]){
-       iterable.splice(i, 1,'qwertasdf')
+    if(iterable[i]!==iterable[i+1]){
+       sumArr.push(iterable[i])
     }
   }
-  return iterable.filter(el=> el!= 'qwertasdf')
+  return sumArr
 }
-console.log(uniqueInOrder('AAAABBBCCDAABBB'))
