a1224ff382ea9a0e72c151b542b00dedb015630a
Pavel <pavel07007@gmail.com>
2023-01-05 12:25:37 +0300

    add one more solution

---
+++ bankomat.js
@@ -0,0 +1,4 @@
+export function getBanknoteList(amountOfMoney) {
+  const banknotes =  [1000, 500, 100, 50, 20, 10, 5, 2, 1]
+  return 
+}
---
+++ factorial.js
@@ -0,0 +1,7 @@
+function factorial(n){
+  let res = 1;
+  for(let i=1;i<=n; i++ ){
+    res *= i
+  }
+  return res
+}
