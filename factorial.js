640a92584071c39eb5f5d26ff277f15fa2527cf6
Pavel <pavel07007@gmail.com>
2023-01-05 22:14:28 +0300

    add one vore factorial solution

--- factorial.js
+++ factorial.js
@@ -1,7 +1,15 @@
-function factorial(n){
-  let res = 1;
-  for(let i=1;i<=n; i++ ){
-    res *= i
-  }
-  return res
+
+
+const sumTo = (x)=>{
+  if(x===1){
+    return x
+  } return x+sumTo(x-1)
 }
+console.log(sumTo(100))
+
+const factorial =(n)=>{
+  if(n === 1){
+    return n
+  } return n*factorial (n-1)
+}
+console.log(factorial(5))
