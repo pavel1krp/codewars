cb55ecf0a60617847de2d22c5e2d03246f369e8e
Pavel <pavel07007@gmail.com>
2023-02-12 18:17:26 +0300

    try to add one more function

--- matrix-transpose.js
+++ matrix-transpose.js
@@ -1,11 +1,4 @@
 function transpose(matrix) {
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < i; j++) {
-       const tmp = matrix[i][j];
-       matrix[i][j] = matrix[j][i];
-       matrix[j][i] = tmp;
-    };
- }
   return matrix
 }
 console.log(transpose([[1,2,3],[4,5,6]]))
