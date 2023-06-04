c25d6d20fcd1aab8b24558857ef4ac862e12a5bf
Pavel <pavel07007@gmail.com>
2023-06-04 08:10:11 +0300

    add zipC solution

--- zipwith.js
+++ zipwith.js
@@ -1,11 +1,12 @@
-const plus = (a,b) => a+b ;
+const plus = (a,b)=> a+b
 function zipWith(fn,a0,a1) {
   let x = []
-  for(let i=0;i<=a0.length; i++){
-    x.push(fn(a0[i],a1[i]))
+  const min = Math.min(a0.length, a1.length)
+    for(let i=0;i<min; i++){
+      x.push(fn(a0[i],a1[i]))
+    }
+    return x;
   }
-  return x;
-}
-console.log((plus, [0,1,2,3,4,5], [6,5,4,3,2,1]), [6,6,6,6,6,6])
+console.log(zipWith(plus,[0,1,2,3,4,5], [6,5,4,3,2,1] ))
 
 console.log(Math.pow(10,3))
